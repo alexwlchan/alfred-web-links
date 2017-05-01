22ad1807b760e0f47877526794fbc54b96d0dc11
Alex Chan <alex@alexwlchan.net>
2017-05-01 15:44:06 +0100

    Build the info.plist + objects correctly

--- build_alfred_workflow.py
+++ build_alfred_workflow.py
@@ -1,26 +1,89 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8
 
+import collections
 import os
 import plistlib
+import uuid
 import zipfile
 
-with zipfile.ZipFile('AWS shortcuts.alfredworkflow', 'w') as package:
-    os.chdir('AWS shortcuts')
+aws_region = 'eu-west-1'
+
+data = {
+    'bundleid': 'com.alexwlchan.aws-shortcuts',
+    'category': 'Internet',
+    'connections': {},
+    'createdby': 'Alex Chan',
+    'description': f'Shortcuts for the AWS console ({aws_region})',
+    'name': 'AWS shortcuts',
+    'objects': [],
+    'readme': '',
+    'uidata': {},
+    'version': '0.0.1',
+    'webaddress': 'https://github.com/alexwlchan/alfred-aws-shortcuts',
+}
+
+aws_resources = sorted([
+    f[:-len('.png')]
+    for f in os.listdir('AWS shortcuts')
+    if f.endswith('.png')
+])
+
+for idx, resource in enumerate(aws_resources):
+    trigger_object = {
+        'config': {
+            'argumenttype': 2,
+            'keyword': resource.lower(),
+            'subtext': '',
+            'text': resource,
+            'withspace': False,
+        },
+        'type': 'alfred.workflow.input.keyword',
+        'uid': str(uuid.uuid4()).upper(),
+        'version': 1,
+    }
+
+    browser_object = {
+        'config': {
+            'browser': '',
+            'spaces': '',
+            'url': f'https://{aws_region}.console.aws.amazon.com/{resource}',
+            'utf8': True,
+        },
+        'type': 'alfred.workflow.action.openurl',
+        'uid': str(uuid.uuid4()).upper(),
+        'version': 1,
+    }
+
+    data['objects'].append(trigger_object)
+    data['objects'].append(browser_object)
+
+    data['uidata'][trigger_object['uid']] = {
+        'xpos': 50 + (400 * (idx % 2)),
+        'ypos': 50 + 300 * (2 * idx % 2),
+    }
+    data['uidata'][browser_object['uid']] = {
+        'xpos': 250 + (400 * (idx % 2)),
+        'ypos': 50 + 300 * (2 * idx % 2),
+    }
+
+    data['connections'][trigger_object['uid']] = [
+        {
+            'destinationuid': browser_object['uid'],
+            'modifiers': 0,
+            'modifiersubtext': '',
+            'vitoclose': False,
+        }
+    ]
+
+
+os.chdir('AWS shortcuts')
+plistlib.writePlist(data, 'info.plist')
+
+os.unlink('../AWS shortcuts.alfredworkflow')
+with zipfile.ZipFile('../AWS shortcuts.alfredworkflow', 'w') as package:
+
     for filename in os.listdir('.'):
         if filename.startswith('.'):
             continue
         package.write(filename)
-
-data = {
-    'bundleid': 'com.alexwlchan.example',
-    'category': 'Tools/Internet/Productivity/Uncategorised',
-    'createdby': 'Alex Chan',
-    'description': 'An example workflow',
-    'name': 'Example',
-    'readme': 'The README goes here',
-    'version': '0.0.1',
-    'webaddress': 'https://example.com',
-}
-
-plistlib.writePlist(data, 'info.plist')
